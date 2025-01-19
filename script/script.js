fd8dfbb32c6cf57ff82b917967d7ccc69dca7da5
Juff-KD <juffcade@gmail.com>
2025-01-19 19:28:40 +0630

    rechg

--- script/script.js
+++
@@ -1,6 +0,0 @@
- fetch('https://raw.githubusercontent.com/Juff-KD/On-Learn/18c750d878a8b53c542593340a6a56a5be16a93c/script/hello.md') 
-            .then(response => response.text())
-            .then(markdown => {
-                const html = marked(markdown); 
-                document.getElementById('markdown-content').innerHTML = html;
-            });
