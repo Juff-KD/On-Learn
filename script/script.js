60a1514a25c1d78159c1b96d80b90dc4980eeec3
Juff-KD <juffcade@gmail.com>
2025-01-19 19:02:38 +0630

    fixMd

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
- fetch('https://raw.githubusercontent.com/Juff-KD/On-Learn/main/script/hello.md') 
+ fetch('https://raw.githubusercontent.com/Juff-KD/On-Learn/18c750d878a8b53c542593340a6a56a5be16a93c/script/hello.md') 
             .then(response => response.text())
             .then(markdown => {
                 const html = marked(markdown); 
